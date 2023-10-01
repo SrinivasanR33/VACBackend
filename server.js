341e541dea9de6b1586fb518481075c793645631
Srinivasan <vasan4649@gmail.com>
2023-10-01 12:44:49 +0530

    Url  change

--- server.js
+++ server.js
@@ -5,23 +5,23 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const socketServer = require("./socketServer");
 const authRoutes = require("./routes/authRoutes");
-const friendInvitationRoutes=require("./routes/friendInvitationRoutes")
+const friendInvitationRoutes = require("./routes/friendInvitationRoutes");
 
 const PORT = process.env.PORT || process.env.API_PORT;
 
-const allowedOrigins = "https://vacservice.onrender.com"
+const allowedOrigins = ["https://vacservice.onrender.com"];
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
-    credentials: true,
-    optionsSuccessStatus: 200
-}
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
 const app = express();
 app.use(express.json());
 app.use(cors(corsOptions));
@@ -29,7 +29,6 @@ app.use(cors(corsOptions));
 // register the routes
 app.use("/api/auth", authRoutes);
 app.use("/api/friend-invitation", friendInvitationRoutes);
-
 
 const server = http.createServer(app);
 socketServer.registerSocketServer(server);
